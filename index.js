b9663f0ffca9bd03dd9d7defc8988c620369026a
Hunt Collings <hunt@collings.co.uk>
2019-01-09 19:00:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const API_KEY = "b0ad7012e63564d7941bfbbeba65f56f"
 function handleFormSubmit() {
   let city = document.getElementById('city').value;
   console.log(city);
+  
   fetchCurrentWeather(city);
   fetchFiveDayForecast(city);
 }
