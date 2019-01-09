220f9c0ece3cd23bc5db4b087925626f14cf00dd
Hunt Collings <hunt@collings.co.uk>
2019-01-09 18:13:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,69 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "b0ad7012e63564d7941bfbbeba65f56f"
 
-function handleFormSubmit(event) {
-  //handle submit event
+function handleFormSubmit() {
+  let city = document.getElementById('city').value;
+  console.log(city);
+  fetchCurrentWeather(city);
+  fetchFiveDayForecast(city);
 }
 
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
+  fetch(`https://api.openweathermap.org/data/2.5/weather?APPid=${API_KEY}&q=${city}`)
+    .then(resp => resp.json())
+    .then(json => displayCurrentWeather(json))
+    
 }
 
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
+let currentKelvin = parseInt(json.main.temp,10);
+let currentCelsius = currentKelvin - 273.15;
+let currentTemp = currentCelsius.toFixed(1);
+document.getElementById("temp").innerHTML = `${currentTemp}`;
+
+let lowKelvin = parseInt(json.main.temp_min,10);
+let lowCelsius = lowKelvin - 273.15;
+let lowTemp = lowCelsius.toFixed(1);
+document.getElementById("low").innerHTML = `${lowTemp}`;
+
+let highKelvin = parseInt(json.main.temp_max,10);
+let highCelsius = highKelvin - 273.15;
+let highTemp = highCelsius.toFixed(1);
+document.getElementById("high").innerHTML = `${highTemp}`;
+
+document.getElementById("humidity").innerHTML = `${json.main.humidity}`;
+
+document.getElementById("cloudCover").innerHTML = `${json.clouds.all}`;
 }
 
 
 function fetchFiveDayForecast(city) {
   //fetch five day forecast data based on city
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?APPid=${API_KEY}&q=${city}`)
+  .then(resp => resp.json())
+  .then(json => displayFiveDayForecast(json))
 }
 
+
+//this bit still needs work
 function displayFiveDayForecast(json) {
+  console.log(json);
   //render five day forecast data to the DOM using provided IDs and json from API
+const aside = document.querySelector('aside')
+
+   for (var dt_txt in json.list) {
+     console.log(json.list[dt_txt])
+   }
+   
+   
 }
 
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  //add event listener here for form submission
-})
+//document.addEventListener('DOMContentLoaded', function() {
+//document.getElementById(cityForm).addEventListener("submit", handleFormSubmit(event) )
+  
+//})
