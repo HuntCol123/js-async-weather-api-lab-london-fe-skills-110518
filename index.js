4fc9654b1b534176efa1aa879326f6c9aa72347e
Hunt Collings <hunt@collings.co.uk>
2019-01-09 18:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const main = document.querySelector('aside')
      
     
    const h2 = document.createElement('p')
-    h2.innerHTML = `<p>Date/Time: ${slot.dt_txt} Temperature: ${slot.main.temp} Humidity: ${slot.main.humidity}</p>`
+    h2.innerHTML = `<p>Date/Time: ${slot.dt_txt}</p> Temperature: ${slot.main.temp} Humidity: ${slot.main.humidity}</p>`
     main.appendChild(h2)
      console.log(slot.main.temp)
      
